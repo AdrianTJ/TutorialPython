87ac6e9a95f8bbf4f78660bfc092e19acbb4456f
AdrianTJ <adrian.tame.jacobo@gmail.com>
2018-09-01 22:56:57 -0500

    Adde comment to Parameter

--- ParameterFunction.py
+++ ParameterFunction.py
@@ -2,4 +2,10 @@ def simple_addition(num1,num2):
     answer = num1 + num2
     print(answer)
 
-simple_addition(5,8)
+simple_addition(5,8) 
+
+''' 
+Can also specify paramter names in the function, like
+simple_addition(num2=5,num1=8)
+to make things more explicit
+'''
